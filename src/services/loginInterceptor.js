2f523af433d828dc636c79a7ce4f0d855fe03f97
AnuragSingh8090 <anuragkumarsingh154@gmail.com>
2026-07-27 11:33:35 +0530

    some changes

--- src/services/loginInterceptor.js
+++ src/services/loginInterceptor.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
@@ -10,16 +9,21 @@ api.interceptors.request.use((config) => {
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
+        config.withCredentials = true
+        config.headers['Source'] = 'Browser'
     }
 
     return config;
 });
 
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.log("Response Interceptor Error : ", error)
+        return Promise.reject(error);
+    }
+);
 
 export default api;
